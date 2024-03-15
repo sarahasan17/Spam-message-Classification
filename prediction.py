abd7a8787d8fe17cf9c4ce7575a5ad39baa21c4d
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-03-15 11:52:38 +0530

    Create prediction.py

---
+++ prediction.py
@@ -0,0 +1,16 @@
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+vectorizer = CountVectorizer()
+
+def predict(data):
+    trans = vectorizer.transform(pd.DataFrame(data)[0])
+    clf = joblib.load('model.sav')
+
+    return clf.predict(trans)
